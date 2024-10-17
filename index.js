d2c7dfa244e06bb3e78ed9084a0decbcbd53bbd8
Ryder-07 <kunals1209@gmail.com>
2024-10-18 00:10:50 +0530

    added basic structure of the app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+
+app.post("/user/signiup",(req,res)=>{
+
+})
+app.post("/user/signin",(req,res)=>{
+    
+})
+app.get("/user/purchases",(req,res)=>{
+    
+})
+app.post("/course/purchase",(req,res)=>{
+    
+})
+app.get("/user/course",(req,res)=>{
+    
+})
+
+app.listen(3000);
